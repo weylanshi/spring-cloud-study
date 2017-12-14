d7143ce553469cddd5b0cf2854e729364bce88bd
weylan <weylanS@outlook.com>
2017-12-14 16:46:38 +0800

    第二次上传

--- spring-cloud-app/src/main/java/com/loc/study/springboot/app/config/CloudConfig.java
+++ spring-cloud-app/src/main/java/com/loc/study/springboot/app/config/CloudConfig.java
@@ -1,4 +1,29 @@
 package com.loc.study.springboot.app.config;
 
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+@Data
+@RefreshScope
+@ConfigurationProperties(prefix = "loc.cloud.config")
 public class CloudConfig {
+    String param1;
+    int param2;
+
+    public String getParam1() {
+        return param1;
+    }
+
+    public void setParam1(String param1) {
+        this.param1 = param1;
+    }
+
+    public int getParam2() {
+        return param2;
+    }
+
+    public void setParam2(int param2) {
+        this.param2 = param2;
+    }
 }
--- spring-cloud-app/src/main/java/com/loc/study/springboot/app/controller/CloudConfigController.java
+++ spring-cloud-app/src/main/java/com/loc/study/springboot/app/controller/CloudConfigController.java
@@ -1,4 +1,39 @@
 package com.loc.study.springboot.app.controller;
 
+import com.loc.study.springboot.app.config.CloudConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+@RequestMapping(value = "/cloud/config")
+@EnableConfigurationProperties(CloudConfig.class)
 public class CloudConfigController {
+
+    @Autowired
+    private CloudConfig cloudConfig;
+
+    @Value(value = "${loc.cloud.controller.param3}")
+    private String param3;
+
+    @GetMapping(value = "/configParam1")
+    public String getConfig1(){
+        return cloudConfig.getParam1();
+    }
+
+    @GetMapping(value = "/configParam2")
+    public int getConfig2(){
+        return cloudConfig.getParam2();
+    }
+
+    @GetMapping(value = "/configParam3")
+    public String getConfig3(){
+        return param3;
+    }
+
 }
